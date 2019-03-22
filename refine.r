d3e256602b8de6d82fe3dcc4a43d4c76630987b7
Lawrence Weinstein <lawrence.e.weinstein@gmail.com>
2019-03-22 09:15:27 -0400

    Added code to correct misspellings in company names

--- refine.r
+++ refine.r
@@ -3,5 +3,17 @@ library(dplyr)
 # Import CSV file
 refine_data <- tbl_df(read.csv("refine_original.csv"))
 
+# Correct company name
+
+# First get each company name as individual first letter,
+# Then correct spelling based on first letter
+refine_data <- refine_data %>% 
+  mutate(company = tolower(company)) %>%
+  mutate(company = substr(company, 1, 1)) %>%
+  mutate(company = case_when(company == "a" ~ "akzo",
+                             company == "p" | company == "f" ~ "phillips",
+                             company == "v" ~ "van houten",
+                             company == "u" ~ "unilever"))
+
 # Export processed data as CSV
 write.csv(refine_data, file = "refine_clean.csv", row.names = FALSE, quote = FALSE)

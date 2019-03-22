79344653a0efa738ba31448f21dcc9fb0b7438e8
Lawrence Weinstein <lawrence.e.weinstein@gmail.com>
2019-03-22 09:39:09 -0400

    Added dummy variables for company

--- refine.r
+++ refine.r
@@ -30,5 +30,16 @@ refine_data <- refine_data %>%
 refine_data <- refine_data %>% 
   unite("full_address", c("address", "city", "country"), sep = ", ", remove = FALSE)
 
+# Create dummy categories for company and product category
+refine_data <- refine_data %>% 
+  mutate(company_phillips = case_when(company == "phillips" ~ 1,
+                                      TRUE ~ 0),
+         company_akzo = case_when(company == "akzo" ~1,
+                                  TRUE ~ 0 ),
+         company_van_houten = case_when(company =="van houten" ~ 1,
+                                        TRUE ~ 0),
+         company_unilever = case_when(company == "unilever" ~ 1,
+                                      TRUE ~ 0))
+
 # Export processed data as CSV
 write.csv(refine_data, file = "refine_clean.csv", row.names = FALSE, quote = FALSE)
